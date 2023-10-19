abed02943558301a51f449e9e93d2167233b5e50
ViniciusPrataKloh <vinicius.prata.kloh@gmail.com>
2023-10-19 13:27:12 -0300

    Price format utils

---
+++ src/lib/priceFormat.ts
@@ -0,0 +1,5 @@
+export function priceFormat(priceInCents: number): string {
+  const priceInReals = (priceInCents / 100).toLocaleString("pt-BR", { style: 'currency', currency: 'BRL' });
+
+  return priceInReals;
+}
